921bc2a5fcef639411e28fe4c57fda5ec966d814
loda26 <mokhaled26498@gmail.com>
2024-01-30 07:31:51 +0200

    task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
+ * Return: number of leaves
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0, left = 0, right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = binary_tree_leaves(tree->left);
+		right = binary_tree_leaves(tree->right);
+		leaves = left + right;
+		return ((!left && !right) ? leaves + 1 : leaves + 0);
+	}
+}
