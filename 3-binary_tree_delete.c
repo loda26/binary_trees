c446c67a6656b6a8dadd3b8a42c9758963241602
loda26 <mokhaled26498@gmail.com>
2024-01-30 05:53:18 +0200

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: tree to free
+ * Return: Nothing
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->right);
+			binary_tree_delete(tree->left);
+		}
+		free(tree);
+	}
+}
