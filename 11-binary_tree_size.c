7c61a0257cfb5c97fdbbd5efa102a36b554d711b
loda26 <mokhaled26498@gmail.com>
2024-01-30 07:32:35 +0200

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,7 +6,7 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0, l = 0, r = 0;
+	size_t size = 0, left = 0, right = 0;
 
 	if (tree == NULL)
 	{
@@ -14,9 +14,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	else
 	{
-		l = binary_tree_size(tree->left);
-		r = binary_tree_size(tree->right);
-		size = l + r + 1;
+		left = binary_tree_size(tree->left);
+		right = binary_tree_size(tree->right);
+		size = left + right + 1;
 	}
 	return (size);
 }
