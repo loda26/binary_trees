61bc5fda6d90092c80e9ee8c7a5be0adfbdb35d7
loda26 <mokhaled26498@gmail.com>
2024-01-30 06:12:49 +0200

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,9 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
