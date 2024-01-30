268977fd0b2a6d7620d46f1ebf26dab887ec1b87
loda26 <mokhaled26498@gmail.com>
2024-01-30 07:33:22 +0200

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0, r = 0;
+	size_t left = 0, right = 0;
 
 	if (tree == NULL)
 	{
@@ -18,9 +18,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((l > r) ? l : r);
+		return ((left > right) ? left : right);
 	}
 }
