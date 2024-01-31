387133b11c412938d4fee227052733ce84e6bb93
loda26 <mokhaled26498@gmail.com>
2024-01-31 06:42:18 +0200

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,19 +8,19 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-    {
-        return (NULL);
-    }
-    else
-    {
-        if (node->parent->left == node)
-        {
-            return (node->parent->right);
-        }
-        else
-        {
-            return (node->parent->left);
-        }
-    }
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (node->parent->left == node)
+		{
+			return (node->parent->right);
+		}
+		else
+		{
+			return (node->parent->left);
+		}
+	}
 }
